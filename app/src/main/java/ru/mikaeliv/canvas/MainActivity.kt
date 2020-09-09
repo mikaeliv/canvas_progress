ef777914b60dcc2678f72050d81b9f8126ff2c9e
Michael <ivanyushchenkov@gemark.ru>
2020-09-09 19:53:42 +0300

    add: tv to show progress

--- app/src/main/java/ru/mikaeliv/canvas/MainActivity.kt
+++ app/src/main/java/ru/mikaeliv/canvas/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
         btnApply.setOnClickListener {
             progress.setProgress(etProgress.text.toString().toFloat())
+            tvProgressCount.text = etProgress.text.toString()
         }
     }
 }
